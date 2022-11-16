cd57aa2a9d90bbafa50e4c1b2ed4ebaec492c98b
unknown <wiercikdoc@gmail.com>
2022-11-16 23:33:18 +0100

    Added react-router to index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from 'views/Root';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import ErrorPage from 'views/ErrorPage/ErrorPage';
+import University from 'views/University/University';
+import Home from 'views/Home/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/university',
+        element: <University />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
