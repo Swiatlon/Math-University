d3469c4b917c53dc98f9181a73f97c1bfca299c1
unknown <wiercikdoc@gmail.com>
2022-11-16 23:35:04 +0100

    Initial view of navbar

---
+++ src/components/organisms/Navigation.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const NavigationWrapper = styled.nav`
+  width: 100%;
+  height: 130px;
+  background: #3679fa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+`;
+export const Logo = styled.img`
+  width: auto;
+  max-height: 120px;
+`;
+export const NavItem = styled.p`
+
+`;
