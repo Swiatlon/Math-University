a66cf9ec52943e3b5cb58e116fb8cb5308fde7ac
unknown <wiercikdoc@gmail.com>
2022-12-25 16:59:07 +0100

    Styled the error page component

--- src/views/ErrorPage/ErrorPage.js
+++ src/views/ErrorPage/ErrorPage.js
@@ -1,16 +1,12 @@
 import { useRouteError } from 'react-router-dom';
+import { ErrorPageHeader } from './ErrorPage.style';
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <ErrorPageHeader>
+      <h1>You've found a page that doesn't exist</h1>
+      <p>Breathe in, and on the out breath, go back and try again.</p>
+    </ErrorPageHeader>
   );
 }
