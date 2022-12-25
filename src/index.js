24e46246a20a5f59cac3c844c825c10d6ebfda2b
unknown <wiercikdoc@gmail.com>
2022-12-25 16:58:29 +0100

    Deleted unnecessary code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from 'views/Root';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from 'views/ErrorPage/ErrorPage';
-import University from 'views/University/University';
 import Home from 'views/Home/Home';
 
 const router = createBrowserRouter([
@@ -17,14 +16,9 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
-      {
-        path: '/university',
-        element: <University />,
-      },
     ],
   },
 ]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- src/views/University/University.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function University() {
-  return <div>University</div>;
-}
-
-export default University;
