f6766887dccbc6e24065d451234028b1bf0540eb
unknown <wiercikdoc@gmail.com>
2022-12-25 16:59:31 +0100

    Recreated the home component due to new design

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Header } from './Home.style';
-import { RightSideTextBox } from './Home.style';
 import { HomeTitle, HomeSubTitle } from './Home.style';
 function Home() {
   return (
     <Header>
-      <RightSideTextBox>
-        <HomeTitle>Quick Math</HomeTitle>
-        <HomeSubTitle>We're here to help you learning!</HomeSubTitle>
-      </RightSideTextBox>
+      <HomeTitle>Quick Math</HomeTitle>
+      <HomeSubTitle>We're here to help you learning!</HomeSubTitle>
     </Header>
   );
 }
--- src/views/Home/Home.style.js
+++ src/views/Home/Home.style.js
@@ -1,27 +1,38 @@
 import styled from 'styled-components';
-import backgroundImage from 'assets/images/backgroundTest.jpg';
-
+// import backgroundImage from 'assets/images/backgroundTest.jpg';
+import mediaImage from 'assets/images/backgroundPhone.jpeg';
+import pcImage from 'assets/images/backgroundPC.jpg';
+import { size } from 'assets/styles/mediaQueries.style';
 export const Header = styled.header`
-  background-image: url(${backgroundImage});
-  background-size: 80%;
-  background-position: left bottom;
+  width: 100%;
+  height: 100vh;
   background-repeat: no-repeat;
-  font-family: 'Gloria Hallelujah', cursive;
-`;
-export const RightSideTextBox = styled.div`
-  width: 50vw;
-  margin-left: auto;
-  height: 100%;
+  background-image: url(${pcImage});
+  background-size: cover;
+  background-position: center 50%;
+  display: grid;
+  align-content: center;
   text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  font-family: 'Gloria Hallelujah', cursive;
+  color: white;
+  text-shadow: 6px 1px 4px #0c2f73;
+  @media (max-width: ${size.tablet}) {
+    background-position: 30% !important;
+  }
+  @media (max-width: ${size.laptop}) {
+    background-image: url(${mediaImage});
+    background-size: cover;
+    background-position: 50%;
+  }
 `;
 export const HomeTitle = styled.h1`
-  font-weight: 400;
-  font-size: 60px;
+  font-size: 48px;
 `;
 export const HomeSubTitle = styled.h2`
-  font-weight: 400;
-  font-size: 30px;
+  text-decoration: underline black;
+  text-underline-offset: 8px;
+  line-height: 40px;
+  @media (max-width: ${size.laptop}) {
+    text-decoration: underline #2f75fc;
+  }
 `;
