c0618966a1514ce6b6d1a1dd77d3aee1f44cb56a
unknown <wiercikdoc@gmail.com>
2023-01-09 20:08:15 +0100

    Added first view of routes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from 'views/Root';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import ErrorPage from 'views/ErrorPage/ErrorPage';
 import Home from 'views/Home/Home';
-
+import SchoolLayout from 'layouts/SchoolLayout';
+import Matrices from 'views/University/Matrices/Matrices';
+import ComplexNumbers from 'views/University/Complex Numbers/ComplexNumbers';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -13,8 +15,49 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: '',
         element: <Home />,
+      },
+      {
+        path: 'Matematyka',
+        element: <SchoolLayout />,
+        // children: [{ path: 'Studia/:subject/:topic', element: <UniversityLayout /> }],
+        children: [
+          {
+            index: true,
+            element: <Navigate to="/error-page" />,
+          },
+          {
+            path: 'Studia',
+            children: [
+              {
+                index: true,
+                element: <Navigate to="/error-page" />,
+              },
+              {
+                path: 'Statystyka-Opisowa',
+                children: [
+                  {
+                    index: true,
+                    element: <Navigate to="/error-page" />,
+                  },
+                  { path: 'Spearmaan', element: <Matrices /> },
+                ],
+              },
+              {
+                path: 'Algebra-Liniowa',
+                children: [
+                  {
+                    index: true,
+                    element: <Navigate to="/error-page" />,
+                  },
+                  { path: 'Macierze', element: <Matrices /> },
+                  { path: 'Liczby-Zespolone', element: <ComplexNumbers /> },
+                ],
+              },
+            ],
+          },
+        ],
       },
     ],
   },
