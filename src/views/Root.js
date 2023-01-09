708f10fc36dc01f5094b5079bfb87f368047202b
unknown <wiercikdoc@gmail.com>
2023-01-09 20:13:30 +0100

    Recreated layout name/view

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/theme/theme';
 import { globalStyle as GlobalStyle } from 'assets/styles/globalStyle';
-import Layout from 'layout/layout';
+import Layout from 'layouts/MainLayout';
 const Root = () => {
   return (
     <ThemeProvider theme={theme}>
