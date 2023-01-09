8e7e6990ac4626b20d65b059bf7963ed19298955
unknown <wiercikdoc@gmail.com>
2023-01-09 20:12:27 +0100

    Added elements with styles for mathematic layout

---
+++ src/components/molecules/TopicCenterDiv/TopicCenterDiv.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+export const TopicCenterDiv = styled.div`
+  box-shadow: 7px 7px 2px 0px rgb(0 0 0 / 75%);
+  width: 530px;
+  margin-top: 10rem;
+  min-height: 80vh;
+  padding-bottom: 40px;
+  background-size: 30px 30px;
+  border: 1px solid lightblue;
+  background-image: linear-gradient(90deg, rgba(173, 216, 230, 0.3) 1px, transparent 1px),
+    linear-gradient(rgba(173, 216, 230, 1) 1px, white 1px);
+`;
