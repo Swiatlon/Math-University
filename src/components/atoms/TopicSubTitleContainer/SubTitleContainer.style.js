057f9426dc5e1a879177fa972c4fd215034b2494
unknown <wiercikdoc@gmail.com>
2023-01-12 19:10:44 +0100

    Deleted unnecessary files

--- src/components/atoms/TopicSubTitleContainer/SubTitleContainer.style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const SubTitleContainer = styled.div`
-  text-align: left;
-  padding-left: 40px;
-  line-height: 50px;
-  padding-bottom: 42px;
-`;
