b381e1052f80141571a0478546ccb0267bcb0e0d
unknown <wiercikdoc@gmail.com>
2023-02-21 12:58:46 +0100

    Added the calculator

--- src/views/University/Complex Numbers/ComplexNumbers.js
+++ src/views/University/Complex Numbers/ComplexNumbers.js
@@ -8,25 +8,67 @@ import overview from 'assets/images/ComplexNumbers.jpg';
 import { ComplexNumberInput } from './ComplexNumbers.style';
 import { useRef, useEffect, useState } from 'react';
 import MathCoordinateSystem from 'helpers/MathCoordinateSystem';
+import math, { evaluate, sqrt, complex, re, phi } from 'mathjs';
 function ComplexNumbers() {
+  const intitialResultState = {
+    result: '',
+    conjugate: '',
+    module: '',
+    argument: '',
+    trigonometricForm: '',
+    re: '',
+    im: '',
+    errorOccuried: false,
+  };
+  const [results, setResults] = useState(intitialResultState);
   // Input
-  const initialState = '';
-  const [inputValue, setInputValue] = useState(initialState);
+  const inputRef = useRef(null);
+  // Typing + Submit handler
+  let timer;
+  function inputHandler(e) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // when typing end
+      const inputValue = inputRef.current.value.replace(/\s/g, '').trim().toLowerCase();
+      try {
+        const result = complex(evaluate(inputValue));
+        const degree = Math.floor(result.toPolar().phi * (180 / 3.14));
+        const phiArg = result.toPolar().phi;
+        const sqrtArg = result.re * result.re + result.im * result.im;
+        const module = `√${sqrtArg} = ${parseFloat(result.toPolar().r).toFixed(2)}`;
+        const trigForm = `√${sqrtArg}(cos(${degree}°) + isin(${degree}°)`;
+        const resultString = `${(Math.cos(phiArg) * sqrtArg + Math.sin(phiArg) * sqrtArg).toFixed(3)}`;
+        setResults({
+          result: resultString,
+          re: Math.round(result.re),
+          im: result.im,
+          conjugate: result.conjugate().format(),
+          argument: `${degree}°`,
+          module: module,
+          trigonometricForm: trigForm,
+        });
+      } catch (error) {
+        if (inputValue.length === 0) {
+          setResults(intitialResultState);
+        } else {
+          setResults({ errorOccuried: true });
+        }
+      }
+    }, 1000);
+  }
   //CoordinateSystem
-  const canvasRef = useRef(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas != null) {
-      const amountOfNumbers = 7;
-      canvas.width = 250;
-      canvas.height = 250;
-      const field = new MathCoordinateSystem(canvas, amountOfNumbers);
-      field.drawMap();
-      // field.drawPoint(1, 1);
-    }
-  }, [canvasRef]);
-  //Submits
-
+  // const canvasRef = useRef(null);
+  // useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   if (canvas != null) {
+  //     const amountOfNumbers = 7;
+  //     canvas.width = 250;
+  //     canvas.height = 250;
+  //     const field = new MathCoordinateSystem(canvas, amountOfNumbers);
+  //     field.drawMap();
+  //     field.drawPoint(2, 2);
+  //   }
+  // }, [canvasRef]);
   return (
     <>
       <TopicTitle>Liczby Zespolone</TopicTitle>
@@ -41,7 +83,7 @@ function ComplexNumbers() {
 
       <TopicSubTitleContainer subTitle="Wytłumaczenie tematu:">
         <TopicSubTitleContent>
-          <img src={overview} alt="overview" />
+          <img src={overview} alt="overview" className="center-img" />
         </TopicSubTitleContent>
       </TopicSubTitleContainer>
 
@@ -58,35 +100,34 @@ function ComplexNumbers() {
             type="text"
             placeholder="Wpisz swoją liczbe zespoloną"
             name="ComplexNumberValue"
-            value={inputValue}
-            onInput={(e) => {
-              setInputValue(e.target.value);
-            }}
+            ref={inputRef}
+            onInput={inputHandler}
           ></ComplexNumberInput>
+          {results.errorOccuried && <p className="error-message">Wrong expression, you need to write it correct!</p>}
           <ComplexNumbersTable>
             <div>
-              Wynik: <p></p>
+              Wynik: <p>{results.result}</p>
             </div>
             <div>
-              Sprzężenie: <p></p>
+              Sprzężenie: <p>{results.conjugate}</p>
             </div>
             <div>
-              Moduł ∣Z∣: <p></p>
+              Moduł ∣Z∣: <p>{results.module}</p>
             </div>
             <div>
-              Argument: <p></p>
+              Argument: <p>{results.argument}</p>
             </div>
             <div>
-              Postać trygonometryczna: <p></p>
+              Postać trygonometryczna: <p>{results.trigonometricForm}</p>
             </div>
             <div>
-              Część rzeczywista - Re(z): <p></p>
+              Część rzeczywista - Re(z): <p>{results.re}</p>
             </div>
             <div>
-              Część urojona - Im(z): <p></p>
+              Część urojona - Im(z): <p>{results.im}</p>
             </div>
           </ComplexNumbersTable>
-          <ComplexNumbersCoordinate ref={canvasRef}></ComplexNumbersCoordinate>
+          {/* <ComplexNumbersCoordinate ref={canvasRef}></ComplexNumbersCoordinate> */}
         </TopicSubTitleContent>
       </TopicSubTitleContainer>
     </>
