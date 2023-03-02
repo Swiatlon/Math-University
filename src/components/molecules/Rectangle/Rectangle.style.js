6ba6b0d48abeca58f7be80605533da6e23e3ba2e
unknown <wiercikdoc@gmail.com>
2023-03-03 00:46:21 +0100

    Rectangle component first version

---
+++ src/components/molecules/Rectangle/Rectangle.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const RectangleContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  margin-top: 20px;
+  justify-content: center;
+  justify-items: center;
+  position: relative;
+`;
