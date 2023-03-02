b4aeb4e43cfc052438e04fbcdc02da3c19981129
unknown <wiercikdoc@gmail.com>
2023-03-02 15:02:43 +0100

    Added switch that show us the content dependent of user choice

--- src/views/HighSchool/GeometricShapes/GeometricShapes.js
+++ src/views/HighSchool/GeometricShapes/GeometricShapes.js
@@ -5,25 +5,61 @@ import TopicSubTitleContainer from 'components/molecules/TopicSubTitleContainer/
 import { FieldsBoxesContainer } from './GeometricShapes.style';
 import { FieldBox } from './GeometricShapes.style';
 import { FieldTopic } from './GeometricShapes.style';
-import square from 'assets/images/GeometricShapes/square.png';
-import rectangle from 'assets/images/GeometricShapes/rectangle.png';
-import triangle from 'assets/images/GeometricShapes/triangle.png';
-import trapeze from 'assets/images/GeometricShapes/trapeze.png';
-import parallelogram from 'assets/images/GeometricShapes/parallelogram.png';
-import rhomb from 'assets/images/GeometricShapes/rhomb.png';
-import circle from 'assets/images/GeometricShapes/circle.png';
-import circleSector from 'assets/images/GeometricShapes/circleSector.png';
+import squareImg from 'assets/images/GeometricShapes/square.png';
+import rectangleImg from 'assets/images/GeometricShapes/rectangle.png';
+import triangleImg from 'assets/images/GeometricShapes/triangle.png';
+import trapezeImg from 'assets/images/GeometricShapes/trapeze.png';
+import parallelogramImg from 'assets/images/GeometricShapes/parallelogram.png';
+import rhombImg from 'assets/images/GeometricShapes/rhomb.png';
+import circleImg from 'assets/images/GeometricShapes/circle.png';
+import circleSectorImg from 'assets/images/GeometricShapes/circleSector.png';
+import { useState } from 'react';
+import Square from 'components/molecules/Square/Square';
+import Rectangle from 'components/molecules/Rectangle/Rectangle';
+import Triangle from 'components/molecules/Triangle/Triangle';
+import Trapeze from 'components/molecules/Trapeze/Trapeze';
+import Parallelogram from 'components/molecules/Parallelogram/Parallelogram';
+import Rhomb from 'components/molecules/Rhomb/Rhomb';
+import Circle from 'components/molecules/Circle/Circle';
+import CircleSector from 'components/molecules/CircleSector/CircleSector';
 function GeometricShapes() {
+  const initialState = 'None';
+  const [choosedShape, setChoosedShape] = useState(null);
+  // Szesciokat // Osmiokat // WIELOKAT SUMA KATOW ITD // DELTOID
   const elements = [
-    { name: 'Kwadrat', url: square },
-    { name: 'Prostokąt', url: rectangle },
-    { name: 'Trójkat', url: triangle },
-    { name: 'Trapez', url: trapeze },
-    { name: 'Równoległobok', url: parallelogram },
-    { name: 'Romb', url: rhomb },
-    { name: 'Koło', url: circle },
-    { name: 'Wycinek Koła', url: circleSector },
+    { name: 'Kwadrat', url: squareImg },
+    { name: 'Prostokąt', url: rectangleImg },
+    { name: 'Trójkat', url: triangleImg },
+    { name: 'Trapez', url: trapezeImg },
+    { name: 'Równoległobok', url: parallelogramImg },
+    { name: 'Romb', url: rhombImg },
+    { name: 'Koło', url: circleImg },
+    { name: 'Wycinek Koła', url: circleSectorImg },
   ];
+
+  const shapes = () => {
+    switch (choosedShape) {
+      case 'Kwadrat':
+        return <Square />;
+      case 'Prostokąt':
+        return <Rectangle />;
+      case 'Trójkąt':
+        return <Triangle />;
+      case 'Trapez':
+        return <Trapeze />;
+      case 'Rownoległobok':
+        return <Parallelogram />;
+      case 'Romb':
+        return <Rhomb />;
+      case 'Koło':
+        return <Circle />;
+      case 'Wycinek Koła':
+        return <CircleSector />;
+      default:
+        return <></>;
+    }
+  };
+
   return (
     <>
       <TopicTitle>Figury Geometryczne</TopicTitle>
@@ -47,12 +83,18 @@ function GeometricShapes() {
         <TopicSubTitleContent>
           <FieldsBoxesContainer>
             {elements.map((box) => (
-              <FieldBox key={box.name}>
+              <FieldBox
+                key={box.name}
+                onClick={() => {
+                  setChoosedShape(box.name);
+                }}
+              >
                 <FieldTopic>{box.name}</FieldTopic>
                 <img src={box.url} alt={`${box.name}`}></img>
               </FieldBox>
             ))}
           </FieldsBoxesContainer>
+          {choosedShape !== null ? shapes() : ''}
         </TopicSubTitleContent>
       </TopicSubTitleContainer>
     </>
