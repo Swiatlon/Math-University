69fe12726b18dc27db5f5f7cda60ae548c7849a1
unknown <wiercikdoc@gmail.com>
2023-03-05 01:19:54 +0100

    Created input component for creating on SVG text

---
+++ src/components/atoms/DynamicInputForImage/DynamicInputForImage.js
@@ -0,0 +1,19 @@
+import React, { forwardRef } from 'react';
+import { DynamicInputBox } from './DynamicInputForImage.style';
+
+const DynamicInputForImage = forwardRef(({ x, y, value }, ref) => {
+  function handleInputChanges(event) {
+    const inputValue = event.target.value;
+    if (/^[0-9]{0,3}$/.test(inputValue)) {
+      ref.current.value = inputValue;
+    } else {
+      ref.current.value = ref.current.value.replace(/[^0-9]{0,3}/g, '');
+    }
+  }
+  return (
+    <DynamicInputBox>
+      <input placeholder={value} type="text" ref={ref} onChange={handleInputChanges} x={x} y={y} maxLength={3} />
+    </DynamicInputBox>
+  );
+});
+export default DynamicInputForImage;
