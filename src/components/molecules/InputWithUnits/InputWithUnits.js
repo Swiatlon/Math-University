a3fb6afed195f06b4fea7e233197a69719146551
unknown <wiercikdoc@gmail.com>
2023-03-05 14:05:39 +0100

    Added option to show without units

--- src/components/molecules/InputWithUnits/InputWithUnits.js
+++ src/components/molecules/InputWithUnits/InputWithUnits.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { InputNumberStyled } from './InputWithUnits.style';
 import { UnitsBox } from './InputWithUnits.style';
 import Units from '../../atoms/Units/Units';
-function InputWithUnits({ value, onChange, placeholder, hasNegativeNumbers, onClick, maxLength, unitRef }) {
+function InputWithUnits({ value, onChange, placeholder, hasNegativeNumbers, onClick, maxLength, unitRef, noUnits }) {
   return (
     <UnitsBox>
-      <Units unitRef={unitRef}></Units>
+      {!noUnits && <Units unitRef={unitRef}></Units>}
       <InputNumberStyled
         type="text"
         value={value}
