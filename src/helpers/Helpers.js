3003e30fc90ba80dc017f6593044dda0a3bc542c
unknown <wiercikdoc@gmail.com>
2023-03-07 23:52:17 +0100

    Added function for counting undefined in array

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -2,3 +2,12 @@ export function transformToDecIfNeeded(number) {
   if (number.toString().includes('.')) return number.toFixed(5);
   else return number;
 }
+
+export function countUndefined(array) {
+  return array.reduce((count, element) => {
+    if (typeof element === 'undefined') {
+      count++;
+    }
+    return count;
+  }, 0);
+}
