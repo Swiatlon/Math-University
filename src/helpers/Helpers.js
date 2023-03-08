abccc58d20d8cfca1778e3aa8f208e9506de9e41
unknown <wiercikdoc@gmail.com>
2023-03-09 00:13:04 +0100

    Added deg/radian switchers

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -1,13 +1,26 @@
-export function transformToDecIfNeeded(number) {
-  if (number.toString().includes('.')) return number.toFixed(5);
+export function transformToDecIfNeeded(number, decimals) {
+  if (!number) return undefined;
+  if (number.toString().includes('.')) return Number(number.toFixed(decimals ?? 0));
   else return number;
 }
 
 export function countUndefined(array) {
   return array.reduce((count, element) => {
-    if (typeof element === 'undefined') {
+    if (typeof element === 'undefined' || element === false) {
       count++;
     }
     return count;
   }, 0);
+}
+
+export function degToRad(degrees) {
+  if (typeof degrees !== 'number' || Number.isNaN(degrees)) return false;
+  var radians = degrees * (Math.PI / 180);
+  return radians;
+}
+
+export function radToDeg(radians) {
+  if (typeof radians !== 'number' || Number.isNaN(radians)) return false;
+  var degrees = radians * (180 / Math.PI);
+  return Math.round(degrees);
 }
