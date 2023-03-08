fc9a184b7969bd2c811a71b389c07ff63df2ce27
unknown <wiercikdoc@gmail.com>
2023-03-09 00:11:19 +0100

    Added new regexp

--- src/components/atoms/DynamicInputForImage/DynamicInputForImage.js
+++ src/components/atoms/DynamicInputForImage/DynamicInputForImage.js
@@ -3,16 +3,17 @@ import { DynamicInputBox } from './DynamicInputForImage.style';
 
 const DynamicInputForImage = forwardRef(({ x, y, value, placeholder }, ref) => {
   function handleInputChanges(event) {
+    const regexp = /^\d{0,3}(\.\d{0,6})?$/;
     const inputValue = event.target.value;
-    if (/^[0-9]{0,3}$/.test(inputValue)) {
+    if (regexp.test(inputValue)) {
       ref.current.value = inputValue;
     } else {
-      ref.current.value = ref.current.value.replace(/[^0-9]{0,3}/g, '');
+      ref.current.value = ref.current.value.replace(regexp, '');
     }
   }
   return (
     <DynamicInputBox>
-      <input placeholder={placeholder} type="text" ref={ref} onChange={handleInputChanges} x={x} y={y} maxLength={3} />
+      <input placeholder={placeholder} type="text" ref={ref} onChange={handleInputChanges} x={x} y={y} maxLength={9} />
     </DynamicInputBox>
   );
 });
