dd430d772a582e09e055f0a93d56a26d9aec2a48
unknown <wiercikdoc@gmail.com>
2023-03-09 22:35:39 +0100

    Refactored code

--- src/components/molecules/Triangle/Triangle.js
+++ src/components/molecules/Triangle/Triangle.js
@@ -10,7 +10,10 @@ import { TriangleContainer } from './Triangle.style';
 import { GeometricButton } from 'components/atoms/GeometricButton/GeometricButton.style';
 import { SubmitContainer } from 'components/atoms/GeometricButton/GeometricButton.style';
 import ResultTable from '../ResultTable/ResultTable';
-import { ReactComponent as EditableSVGText } from 'assets/images/GeometricShapes/triangleBasic.svg';
+import { ReactComponent as SvgBasic } from 'assets/images/GeometricShapes/triangleBasic.svg';
+import { ReactComponent as SvgIsoceles } from 'assets/images/GeometricShapes/triangleIsosceles.svg';
+import { ReactComponent as SvgEquilateral } from 'assets/images/GeometricShapes/triangleEquilateral.svg';
+import { ReactComponent as SvgRectangular } from 'assets/images/GeometricShapes/triangleRectangular.svg';
 import DynamicInputForImage from 'components/atoms/DynamicInputForImage/DynamicInputForImage';
 import { wrongDataAlert } from 'helpers/Swal';
 import InputWithUnits from '../InputWithUnits/InputWithUnits';
@@ -77,73 +80,115 @@ function Triangle() {
     imagesInputsRefs.current = [];
   };
 
-  const dataCalculation = (a, b, c, alfa, beta, gamma, ha, hb, hc, r, circuit, field, R) => {
+  const dataCalculation = (a, b, c, alfa, beta, gamma, ha, hb, hc, r, circuit, field, R, triangleVersion) => {
+    //Prepare data
     // Degree -> radians cause of tryg functions like sin/cos/tg
     gamma = degToRad(gamma);
     beta = degToRad(beta);
     alfa = degToRad(alfa);
     let oldAmountOfUndefined;
     let actuaAmountOfUndefined;
-    if ((gamma ?? 0) + (beta ?? 0) + (alfa ?? 0) <= Math.PI) {
-      do {
-        oldAmountOfUndefined = countUndefined([a, b, c, alfa, beta, gamma, ha, hb, hc, r, circuit, field, R]);
-        a = a || TriangleFunctions.gettingSideWithAllFunctions(alfa, ha, b, beta, c, gamma, circuit, field, r, R);
-        b = b || TriangleFunctions.gettingSideWithAllFunctions(beta, hb, a, alfa, c, gamma, circuit, field, r, R);
-        c = c || TriangleFunctions.gettingSideWithAllFunctions(gamma, hc, a, alfa, b, beta, circuit, field, r, R);
-        alfa = alfa || TriangleFunctions.gettingAngleAllFunctions(a, b, c, beta, gamma);
-        beta = beta || TriangleFunctions.gettingAngleAllFunctions(b, a, c, alfa, gamma);
-        gamma = gamma || TriangleFunctions.gettingAngleAllFunctions(c, a, b, alfa, beta);
-        ha = ha || TriangleFunctions.getHeight(field, a);
-        hb = hb || TriangleFunctions.getHeight(field, b);
-        hc = hc || TriangleFunctions.getHeight(field, c);
-        R = TriangleFunctions.gettingCircumscribedRadius(a, alfa, b, beta, c, gamma, field);
-        r = r || TriangleFunctions.gettingInscribedRadius(field, a, b, c);
-        circuit = circuit || TriangleFunctions.gettingCircuit(a, b, c);
-        field = field || TriangleFunctions.gettingField(a, ha, alfa, b, hb, beta, c, hc, gamma, r, R);
-        actuaAmountOfUndefined = countUndefined([a, b, c, alfa, beta, gamma, ha, hb, hc, r, circuit, field, R]);
-      } while (oldAmountOfUndefined !== actuaAmountOfUndefined);
 
-      setResult({
-        a: a,
-        b: b,
-        c: c,
-        alfa: radToDeg(alfa),
-        beta: radToDeg(beta),
-        gamma: radToDeg(gamma),
-        ha: ha,
-        hb: hb,
-        hc: hc,
-        r: r,
-        R: R,
-        circuit: circuit,
-        field: field,
-      });
-    } else {
-      wrongDataAlert('Przekroczono maksymalna rozpietość kątów ');
+    switch (triangleVersion) {
+      case 'Prostokatny':
+        gamma = Math.PI / 2; // 90 deg
+        break;
+      case 'Rownoramienny':
+        c = b;
+        gamma = beta;
+        break;
+      case 'Rownoboczny':
+        b = a;
+        c = a;
+        alfa = 1.047197551197; //60 deg x 3
+        beta = 1.047197551197;
+        gamma = 1.047197551197;
+        break;
+      default:
     }
+    // Do Calculations
+    do {
+      oldAmountOfUndefined = countUndefined([a, b, c, alfa, beta, gamma, ha, hb, hc, r, circuit, field, R]);
+      a = a || TriangleFunctions.gettingSideWithAllFunctions(alfa, ha, b, beta, c, gamma, circuit, field, r, R);
+      b = b || TriangleFunctions.gettingSideWithAllFunctions(beta, hb, a, alfa, c, gamma, circuit, field, r, R);
+      c = c || TriangleFunctions.gettingSideWithAllFunctions(gamma, hc, a, alfa, b, beta, circuit, field, r, R);
+      alfa = alfa || TriangleFunctions.gettingAngleAllFunctions(a, b, c, beta, gamma, R);
+      beta = beta || TriangleFunctions.gettingAngleAllFunctions(b, a, c, alfa, gamma, R);
+      gamma = gamma || TriangleFunctions.gettingAngleAllFunctions(c, a, b, alfa, beta, R);
+      ha = ha || TriangleFunctions.getHeight(field, a);
+      hb = hb || TriangleFunctions.getHeight(field, b);
+      hc = hc || TriangleFunctions.getHeight(field, c);
+      R = R || TriangleFunctions.getCircumscribedRadius(a, alfa, b, beta, c, gamma, field);
+      r = r || TriangleFunctions.getInscribedRadius(field, a, b, c);
+      circuit = circuit || TriangleFunctions.getCircuit(a, b, c);
+      field = field || TriangleFunctions.getField(a, ha, alfa, b, hb, beta, c, hc, gamma, r, R);
+      actuaAmountOfUndefined = countUndefined([a, b, c, alfa, beta, gamma, ha, hb, hc, r, circuit, field, R]);
+    } while (oldAmountOfUndefined !== actuaAmountOfUndefined);
+    alfa = radToDeg(alfa);
+    beta = radToDeg(beta);
+    gamma = radToDeg(gamma);
+    return [a, b, c, alfa, beta, gamma, ha, hb, hc, r, R, circuit, field];
   };
 
   const submitData = () => {
-    const imagesInputsValues = imagesInputsRefs.current.map((ref) => {
-      return {
-        name: ref.current?.attributes[0]?.value, // placeholder
-        value: ref.current.value,
-      };
+    const variables = ['a', 'b', 'c', 'α', 'β', 'Y', 'h(a)', 'h(b)', 'h(c)', 'r', 'R', 'circuit', 'field'];
+
+    const values = variables.map((variable) => {
+      const value = Number(
+        imagesInputsRefs.current.find((element) => element.current.attributes[0].value === variable)?.current.value
+      );
+      return value > 0 ? value : false;
     });
-    const isDataValid = imagesInputsValues.every((item) => /^\d{0,3}(\.\d{0,6})?$/.test(item?.value));
-    if (!isDataValid) wrongDataAlert('Dane są nieprawidłowe');
-    else {
-      const variables = ['a', 'b', 'c', 'α', 'β', 'Y', 'h(a)', 'h(b)', 'h(c)', 'r'];
-      const values = variables.map((variable) => {
-        const value = Number(
-          imagesInputsRefs.current.find((element) => element.current.attributes[0].value === variable)?.current.value
-        );
-        return value > 0 ? value : false;
-      });
-      const userData = Object.values(preResultInputs).map((item) => (item > 0 ? Number(item) : false));
-      dataCalculation(...values, ...userData);
+    // After set items we change  α to alfa and same for beta
+
+    variables[3] = 'alfa';
+    variables[4] = 'beta';
+
+    const userData = Object.values(preResultInputs).map((item) => (item > 0 ? Number(item) : false));
+    const calculatedValues = dataCalculation(...values, ...userData, choosedTriangleVersion).reduce(
+      (acc, item, index) => {
+        acc[variables[index]] = item;
+        return acc;
+      },
+      {}
+    );
+
+    // Checking if everything is correct
+    if (calculatedValues.alfa * 1 + calculatedValues.beta * 1 + calculatedValues.Y * 1 > 180) {
+      wrongDataAlert('Zbyt wielka rozpietość kątów');
+      return setResult({ a: 'Niepoprawne Dane' });
     }
-    // All the calculations
+
+    const wrongSideValues =
+      calculatedValues.a > calculatedValues.b + calculatedValues.c ||
+      calculatedValues.b > calculatedValues.a + calculatedValues.c ||
+      calculatedValues.c > calculatedValues.a + calculatedValues.b;
+
+    if (wrongSideValues) {
+      wrongDataAlert('Niepoprawna długość boków trójkata!');
+      return setResult({ a: 'Niepoprawne Dane' });
+    }
+
+    switch (choosedTriangleVersion) {
+      case 'Zwykly':
+        break;
+      case 'Prostokatny':
+        calculatedValues.Y = false; // 90 deg
+        break;
+      case 'Rownoramienny':
+        calculatedValues.c = false;
+        calculatedValues.Y = false;
+        break;
+      case 'Rownoboczny':
+        calculatedValues.b = false;
+        calculatedValues.beta = false;
+        calculatedValues.c = false;
+        calculatedValues.Y = false;
+        break;
+      default:
+    }
+
+    console.log(calculatedValues);
   };
 
   const elements = [
@@ -155,15 +200,14 @@ function Triangle() {
 
   const shape = () => {
     switch (choosedTriangleVersion) {
-      case 'Zwykły':
-        return <EditableSVGText onClick={creatingInputsOnText}> </EditableSVGText>;
-
-      case 'Prostokąt':
-        return 0;
-      case 'Trójkąt':
-        return 0;
-      case 'Trapez':
-        return 0;
+      case 'Zwykly':
+        return <SvgBasic onClick={creatingInputsOnText}> </SvgBasic>;
+      case 'Rownoramienny':
+        return <SvgIsoceles onClick={creatingInputsOnText}> </SvgIsoceles>;
+      case 'Rownoboczny':
+        return <SvgEquilateral onClick={creatingInputsOnText}> </SvgEquilateral>;
+      case 'Prostokatny':
+        return <SvgRectangular onClick={creatingInputsOnText}> </SvgRectangular>;
       default:
         return <></>;
     }
@@ -179,7 +223,11 @@ function Triangle() {
             key={box.name}
             elemPerRow={2.7}
             onClick={() => {
-              setChoosedTriangleVersion(box.name);
+              // Normalize text
+              let boxName = box.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+              // Change polish chars
+              boxName = boxName.replace(/[ł]/g, 'l');
+              setChoosedTriangleVersion(boxName);
               resetData();
             }}
           >
@@ -239,11 +287,15 @@ function Triangle() {
           </SubmitContainer>
 
           <ResultTable>
-            {Object.entries(result).map(([key, value]) => (
-              <div key={key}>
-                {key}:<p>{transformToDecIfNeeded(value, 2)}</p>
-              </div>
-            ))}
+            {Object.entries(result).map(([key, value]) =>
+              value ? (
+                <div key={key}>
+                  {key}:<p>{transformToDecIfNeeded(value, 2)}</p>
+                </div>
+              ) : (
+                ''
+              )
+            )}
           </ResultTable>
         </>
       ) : (
